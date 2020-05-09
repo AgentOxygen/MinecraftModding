b8ed78e3f7ed9d3441b3b8857b4b7af820b6195d
Cameron <cameronscummins@gmail.com>
2020-05-09 18:05:56 -0500

    Added comment

--- src/main/java/com/mntchkn/will/WillsItems.java
+++ src/main/java/com/mntchkn/will/WillsItems.java
@@ -2,7 +2,9 @@ package com.mntchkn.will;
 
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
-
+/*
+ * Holds registry methods for adding Will's items
+ */
 public class WillsItems {
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		

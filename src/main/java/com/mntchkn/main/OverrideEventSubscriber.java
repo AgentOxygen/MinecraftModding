a91f3c2df658258bd625df1ac038f7c99f57c0ab
Cameron <cameronscummins@gmail.com>
2020-05-10 16:03:20 -0500

    Added Questing/Task system base

--- src/main/java/com/mntchkn/main/OverrideEventSubscriber.java
+++ src/main/java/com/mntchkn/main/OverrideEventSubscriber.java
@@ -1,14 +1,13 @@
 package com.mntchkn.main;
 
-import java.util.Map;
-
-import com.google.common.collect.Maps;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import net.minecraft.advancements.Advancement;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 
 /* Overrides Specific Vanilla features and aspects
@@ -19,15 +18,23 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.FORGE)
 public class OverrideEventSubscriber {
 	
-	private final static Map<ResourceLocation, Advancement> advancements = Maps.newHashMap();
 	
+	// Removes all non-mod advancements (advancements without Main.MODID modid
 	@SubscribeEvent
 	public static void onServerStarting(FMLServerStartingEvent event) {
-		Main.LOGGER.debug("Deleting vanilla advancements...");
-		//event.getServer().advancementManager.field_223388_c.clear();
-		event.getServer().advancementManager.field_223388_c.clear();
-		for(int i = 0; i < event.getServer().advancementManager.field_223388_c.getAll().size(); i++) {
-			Main.LOGGER.debug(event.getServer().advancementManager.field_223388_c.getAll().toArray()[i]);
+		Main.LOGGER.debug("Removing vanilla advancements...");
+		Iterator<Advancement> iteration = event.getServer().advancementManager.field_223388_c.getAll().iterator();
+		Set<ResourceLocation> ids = new HashSet<>();
+		
+		for(int i = 0; i< event.getServer().advancementManager.field_223388_c.getAll().size(); i++) {
+			
+			Advancement advancement = iteration.next();
+			
+			if(!advancement.getId().toString().contains(Main.MODID) && advancement.getId().toString() != null) {
+				ids.add(advancement.getId());
+			}
 		}
+		
+		event.getServer().advancementManager.field_223388_c.removeAll(ids);
 	}
 }

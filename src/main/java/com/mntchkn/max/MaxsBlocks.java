fce77835806fec33dfdf301df7173694f70740dc
Cameron <cameronscummins@gmail.com>
2020-05-10 16:05:17 -0500

    Cleaned up imports

--- src/main/java/com/mntchkn/max/MaxsBlocks.java
+++ src/main/java/com/mntchkn/max/MaxsBlocks.java
@@ -3,10 +3,6 @@ package com.mntchkn.max;
 import com.mntchkn.main.Main;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraftforge.common.ToolType;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 /*
--- src/main/java/com/mntchkn/max/MaxsItems.java
+++ src/main/java/com/mntchkn/max/MaxsItems.java
@@ -1,13 +1,7 @@
 package com.mntchkn.max;
 
-import com.mntchkn.init.ModItemGroups;
-import com.mntchkn.main.ModEventSubscriber;
-
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.RegistryObject;
 
 /*
  * Holds registry methods for adding Cameron's items

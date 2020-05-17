2e17fedeb54a5108126560d9b3a40a5cb855e706
Cameron <cameronscummins@gmail.com>
2020-05-17 17:37:44 -0500

    Updated Skills

--- src/main/java/com/mntchkn/cam/SkillEventSubscriber.java
+++ src/main/java/com/mntchkn/cam/SkillEventSubscriber.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 import com.mntchkn.main.Main;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.advancements.AdvancementsScreen;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent.LoggedInEvent;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
@@ -19,6 +22,9 @@ public class SkillEventSubscriber {
 	 */
 	public static ArrayList<PlayerDataPackage> player_stats = new ArrayList<PlayerDataPackage>();
 	
+	/**
+	 * Creates player data package for new players.
+	 */
 	@SubscribeEvent
 	public static void onPlayerLogin(LoggedInEvent event) {
 		boolean new_player = true;
@@ -29,6 +35,16 @@ public class SkillEventSubscriber {
 		}
 		if(new_player) {
 			player_stats.add(new PlayerDataPackage(event.getPlayer().getName().toString()));
+		}
+	}
+	
+	/**
+	 * Adds Skill GUI
+	 */
+	@SubscribeEvent
+	public static void onGuiOpen(GuiOpenEvent event) {
+		if(event.getGui().getClass().equals(AdvancementsScreen.class)) {
+			Main.LOGGER.debug("Opened advancements!");
 		}
 	}
 }

3b3875184f8835a57e799155cce8d27534071cf7
Cameron <cameronscummins@gmail.com>
2020-05-17 23:13:12 -0500

    Revised register function

--- src/main/java/com/mntchkn/init/ModEntities.java
+++ src/main/java/com/mntchkn/init/ModEntities.java
@@ -1,23 +1,19 @@
 package com.mntchkn.init;
 
 import com.mntchkn.entities.TestEntity;
-import com.mntchkn.main.Main;
-import com.mntchkn.main.ModEventSubscriber;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraft.world.biome.Biomes;
 
 
 
 public class ModEntities {
-	public static EntityType<?> TEST_ENTITY = EntityType.Builder.create(TestEntity::new, EntityClassification.CREATURE).build(Main.MODID + ":test_entity").setRegistryName(ModEventSubscriber.location("test entity"));
-	
-	
-	
+	public static EntityType<TestEntity> TEST_ENTITY = register("test_entity", EntityType.Builder.create(TestEntity::new, EntityClassification.CREATURE));
 	
 	//World Spawning for Entities
 	public static void registerEntityWorldSpawn() {
@@ -29,4 +25,10 @@ public class ModEntities {
 			biome.getSpawns(entity.getClassification()).add(new SpawnListEntry(entity, 50, 5, 15)); //entity, how often it will spawn, min spawned, max spawned
 		}
 	}
+	
+	private static <T extends Entity> EntityType<T> register(String key, EntityType.Builder<T> builder) {
+		return Registry.register(Registry.ENTITY_TYPE, key, builder.build(key));
+	}
+	
+	
 }

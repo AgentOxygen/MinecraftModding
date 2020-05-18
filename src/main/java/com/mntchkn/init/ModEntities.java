47a1533962f62f80a84f338a5e080c8dcb4c5432
Cameron <cameronscummins@gmail.com>
2020-05-18 00:56:19 -0500

    Added for handling entity registering

--- src/main/java/com/mntchkn/init/ModEntities.java
+++ src/main/java/com/mntchkn/init/ModEntities.java
@@ -1,34 +1,27 @@
 package com.mntchkn.init;
 
-import com.mntchkn.entities.TestEntity;
-import com.mntchkn.entities.TestRenderer;
+import com.mntchkn.entities.CamelEntity;
+import com.mntchkn.main.Main;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
-import net.minecraft.world.biome.Biomes;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 
 public class ModEntities {
-	public static EntityType<TestEntity> TEST_ENTITY = register("test_entity", EntityType.Builder.create(TestEntity::new, EntityClassification.CREATURE));
 	
-	//World Spawning for Entities
-	public static void registerEntities(final RegistryEvent.Register<EntityType<?>> event) {
-		
-		event.getRegistry().registerAll(
-				ModEntities.TEST_ENTITY
-		);
-		
-		
-		registerEntityWorldSpawn(TEST_ENTITY, Biomes.BADLANDS, Biomes.MOUNTAINS);
-	}
+	public static final DeferredRegister<EntityType<?>> ENTITIES = new DeferredRegister<>(ForgeRegistries.ENTITIES, Main.MODID);
+	
+	private static final String camel_name = "camel";
+	public static final RegistryObject<EntityType<CamelEntity>> camel = ENTITIES.register(camel_name, 
+			() -> EntityType.Builder.<CamelEntity>create(CamelEntity::new, EntityClassification.CREATURE)
+				.size(EntityType.HORSE.getWidth(), EntityType.HORSE.getHeight())
+				.build(new ResourceLocation(Main.MODID, camel_name).toString()));
 	
 	//World Spawning for Entities
 	public static void registerEntityWorldSpawn(EntityType<?> entity,Biome...biomes) {
@@ -36,9 +29,4 @@ public class ModEntities {
 			biome.getSpawns(entity.getClassification()).add(new SpawnListEntry(entity, 50, 5, 15)); //entity, how often it will spawn, min spawned, max spawned
 		}
 	}
-	
-	private static <T extends Entity> EntityType<T> register(String key, EntityType.Builder<T> builder) {
-		return Registry.register(Registry.ENTITY_TYPE, key, builder.build(key));
-	}
-	
 }
